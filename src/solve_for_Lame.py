3c6301d5b554fa114b64b40246c09ca79aee2623
Wietse Boon <wietseboon@gmail.com>
2024-07-09 17:14:30 +0200

    updated for multiple cells

--- src/solve_for_Lame.py
+++ src/solve_for_Lame.py
@@ -10,25 +10,33 @@ def labda(rho, kappa=0.25):
     return kappa - 0.5 * mu(rho)
 
 
-if __name__ == "__main__":
-    """ Given a deviatoric stress, find the Lamé parameters """
-
-    # Given norm of the deviatoric stress 
-    dev_s = np.random.rand()
-
+def find_rho(dev_s):
+    """
+    Given the norm of the deviatoric stress,
+    find the norm of the deviatoric strain.
+    """
+    # Set up nonlinear problem
+    func = lambda x: 2 * mu(x) * x - dev_s
     # Initial guess asserts that mu(rho) is close to mu(0)
     x0 = dev_s / (2 * mu(0))
-
-    # Set up nonlinear problem
-    nonlinear_func = lambda x: 2 * mu(x) * x - dev_s
-
     # Use scipy to find root
-    iterator = spo.root_scalar(nonlinear_func, x0=x0)
-    rho = iterator.root
+    result = spo.root_scalar(func, x0=x0)
+    # assert result.converged
 
-    assert iterator.converged
+    return result.root
+
+
+if __name__ == "__main__":
+    # Take a random distribution of deviatoric stresses in the cells
+    n_cells = 100
+    dev_s = 100 * np.random.rand(n_cells)
+
+    # Compute the different rhos
+    rho = np.array([find_rho(s) for s in dev_s])
+
+    # Sanity check
     assert np.allclose(dev_s, 2 * mu(rho) * rho)
 
-    print("Converged in {} iterations".format(iterator.iterations))
-    print("mu: {}".format(mu(rho)))
-    print("lambda: {}".format(labda(rho)))
+    # Retrieve Lamé parameters
+    Lame_mu = mu(rho)
+    Lame_la = labda(rho)
